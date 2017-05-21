fb749e06c6ef35dcbc170439697eed05e64a0455
fanhl <dyguests@qq.com>
2017-05-21 11:35:31 +0800

    format code

--- app/src/main/java/com/fanhl/as3_0/MainActivity.kt
+++ app/src/main/java/com/fanhl/as3_0/MainActivity.kt
@@ -7,32 +7,30 @@ import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
-    private var mTextMessage: TextView? = null
-
-    private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
-        when (item.itemId) {
-            R.id.navigation_home -> {
-                mTextMessage!!.setText(R.string.title_home)
-                return@OnNavigationItemSelectedListener true
-            }
-            R.id.navigation_dashboard -> {
-                mTextMessage!!.setText(R.string.title_dashboard)
-                return@OnNavigationItemSelectedListener true
-            }
-            R.id.navigation_notifications -> {
-                mTextMessage!!.setText(R.string.title_notifications)
-                return@OnNavigationItemSelectedListener true
-            }
-        }
-        false
-    }
+    private var message_tv: TextView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        mTextMessage = findViewById(R.id.message) as TextView
+        message_tv = findViewById(R.id.message_tv) as TextView
         val navigation = findViewById(R.id.navigation) as BottomNavigationView
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+        navigation.setOnNavigationItemSelectedListener(BottomNavigationView.OnNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.navigation_home -> {
+                    message_tv!!.setText(R.string.title_home)
+                    return@OnNavigationItemSelectedListener true
+                }
+                R.id.navigation_dashboard -> {
+                    message_tv!!.setText(R.string.title_dashboard)
+                    return@OnNavigationItemSelectedListener true
+                }
+                R.id.navigation_notifications -> {
+                    message_tv!!.setText(R.string.title_notifications)
+                    return@OnNavigationItemSelectedListener true
+                }
+            }
+            false
+        })
     }
 }
