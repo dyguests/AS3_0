fcce9241074f4acba5684768d4c9d136f0fdce71
fanhl <dyguests@qq.com>
2017-05-26 11:07:26 +0800

    demo

--- app/src/main/java/com/fanhl/as3_0/MainActivity.kt
+++ app/src/main/java/com/fanhl/as3_0/MainActivity.kt
@@ -4,29 +4,30 @@ import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
 import android.support.v7.app.AppCompatActivity
 import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.msg_tv
 
 class MainActivity : AppCompatActivity() {
 
-    private var message_tv: TextView? = null
+    private var msg_tv: TextView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        message_tv = findViewById(R.id.message_tv) as TextView
+        msg_tv = findViewById(R.id.msg_tv) as TextView
         val navigation = findViewById(R.id.navigation) as BottomNavigationView
         navigation.setOnNavigationItemSelectedListener(BottomNavigationView.OnNavigationItemSelectedListener {
             when (it.itemId) {
                 R.id.navigation_home -> {
-                    message_tv!!.setText(R.string.title_home)
+                    msg_tv!!.setText(R.string.title_home)
                     return@OnNavigationItemSelectedListener true
                 }
                 R.id.navigation_dashboard -> {
-                    message_tv!!.setText(R.string.title_dashboard)
+                    msg_tv!!.setText(R.string.title_dashboard)
                     return@OnNavigationItemSelectedListener true
                 }
                 R.id.navigation_notifications -> {
-                    message_tv!!.setText(R.string.title_notifications)
+                    msg_tv!!.setText(R.string.title_notifications)
                     return@OnNavigationItemSelectedListener true
                 }
             }
